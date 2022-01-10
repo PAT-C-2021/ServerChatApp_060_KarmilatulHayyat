f6c3017dcba2e22da668abecd22304cae42a21d7
Karmilatul Hayyat <63487509+KarmilatulHayyat@users.noreply.github.com>
2022-01-10 22:17:35 +0700

    Add project files.

---
+++ ServerChatApp_060_KarmilatulHayyat/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WCFService_2Way_060;
+
+namespace ServerChatApp_060_KarmilatulHayyat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(ServiceCallBack));
+                hostObj.Open();
+                Console.WriteLine("Server Ready Dungsss!!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
